a00deeda42a9bd4ea8d7af7cac78947c325f24c3
Matthew Rueben <ruebenm@onid.oregonstate.edu>
2014-11-20 21:02:27 -0800

    Location class. Testing for Location class.

--- classes/environment.py
+++ classes/environment.py
@@ -8,7 +8,23 @@ class POI():
     pass
 
 
-class Location():
-    pass
+import math
 
+class Location:
+        def __init__(self, x, y):
+                self.x = x
+                self.y = y
+
+        def __str__(self):
+                return 'Location: (' + str(self.x) + ',' + str(self.y) + ')'
+
+        def __eq__(self, other):
+                if isinstance(other, Location):
+                        if self.x == other.x and self.y == other.y:
+                                return True
+                return False
+
+        def __sub__(self, subtrahend):
+            """ Location subtraction = Euclidean distance between the two. """
+            return math.sqrt((self.x - subtrahend.x) ** 2 + (self.y - subtrahend.y) ** 2)
 
--- scripts/test/test_environment.py
+++ scripts/test/test_environment.py
@@ -7,4 +7,13 @@ sys.path.append('/nfs/attic/smartw/users/ruebenm/workspaces/exploration/src/clas
 from environment import World, POI, Location
 
 if __name__ == "__main__":
-    pass
+    # Test Location class
+    a = Location(x=1, y=-3)
+    b = Location(x=-1, y=1)
+    c = Location(x=-1, y=1)
+    print a
+    print 'Should be 4.47:', a - b
+    print 'Should be True:', b == c
+
+    
+               
