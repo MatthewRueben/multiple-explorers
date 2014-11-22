fbd6b4cdde1ffa1889eb4dfcf069b3f59e7661e8
Matthew Rueben <ruebenm@onid.oregonstate.edu>
2014-11-21 18:28:36 -0800

    Global reward calculation works.

--- classes/environment.py
+++ classes/environment.py
@@ -5,29 +5,67 @@ import random
 from matplotlib import pyplot
 
 class World():
-    def __init__(self, world_bounds, N_poi, poi_bounds):
+    def __init__(self, world_bounds, N_poi, poi_bounds, N_rovers, rover_start):
         """ Inputs "world_bounds" and "poi_bounds" are of class "2DBounds". """
         V_bounds = (0.0, 1.0)  # FIX ME?
         self.world_bounds = world_bounds
         self.poi_bounds = poi_bounds
+
+        # Init POIs
         self.POIs = []
         for poi_index in range(N_poi):
             V_choice = random.uniform(V_bounds[0], V_bounds[1])
-            poi = POI(V_choice)  # assign POI value
+            poi = POI(V_choice, d_min=5.0)  # assign POI value & minimum observation distance
             self.POIs.append(poi)
+
+        # Init rovers
+        self.rovers = []
+        for rover_index in range(N_rovers):
+            dx = random.uniform(-35, 35)  # FOR TESTING ONLY!
+            dy = random.uniform(-35, 35)  # FOR TESTING ONLY
+            rover = DummyRover(rover_start.x + dx, 
+                               rover_start.y + dy)  # FOR TESTING ONLY!
+            self.rovers.append(rover)
         
     def reset(self):
         for poi in self.POIs:
             poi.place_randomly(self.poi_bounds)  # assign POI location
+            
+    def get_rewards(self):
+        reward = 0
+        rover_closest_list = []
+        for poi in self.POIs:
+            delta_min = 100.0  # start arbitrarily high
+            rover_closest = None
+            for rover_index, rover in enumerate(self.rovers):
+                delta = (rover.location - poi)  # observation distance
+                if delta < delta_min:
+                    delta_min = delta
+                    rover_closest = rover_index
+            delta_min = min(delta_min ** 2, poi.d_min ** 2)  # delta is actually the SQUARED Euclidean distance
+            rover_closest_list.append(rover_closest)
+            reward += poi.V / delta_min
+        return reward, rover_closest_list
 
-    def test_plot(self):
+    def test_plot(self, rover_closest_list=[]):
         pyplot.ion()
         pyplot.cla()
         for poi in self.POIs:
             pyplot.plot(poi.location.x, poi.location.y, 'k*')
+        for rover in self.rovers:
+            pyplot.plot(rover.location.x, rover.location.y, 'ro')
         pyplot.axis([self.world_bounds.x_lower, self.world_bounds.x_upper, 
                      self.world_bounds.y_lower, self.world_bounds.y_upper])
+        if len(rover_closest_list) > 0:
+            for poi_index, rover_closest in enumerate(rover_closest_list):
+                pyplot.plot([self.rovers[rover_closest].location.x, self.POIs[poi_index].location.x],
+                            [self.rovers[rover_closest].location.y, self.POIs[poi_index].location.y])
         pyplot.draw()
+
+
+class DummyRover():
+    def __init__(self, x, y):
+        self.location = Location(x, y)
 
 
 import random
@@ -54,23 +92,13 @@ class Bounds2D():
         return str(((self.x_lower, self.x_upper), 
                     (self.y_lower, self.y_upper)))
 
-    #def __add__(self, addend):
-    #    """ Addition for 2DBounds classes is just attribute-by-attribute. """
-    #    x_lower = self.x_lower + addend.x_lower
-    #    x_upper = self.x_upper + addend.x_upper
-    #
-    #    y_lower = self.y_lower + addend.y_lower
-    #    y_upper = self.y_upper + addend.y_upper
-    #
-    #    return 2DBounds((x_lower, x_upper), (y_lower, y_upper))
-        
-
 
 import random
 
 class POI():
-    def __init__(self, V):
+    def __init__(self, V, d_min):
         self.V = V  # POI value
+        self.d_min = d_min
 
     def place_randomly(self, bounds):
         x, y  = bounds.random_within_bounds()
@@ -99,8 +127,15 @@ class Location:
                 return True
             return False
 
-    def __sub__(self, other_location):
+    def __sub__(self, subtrahend):
         """ Subtraction for Locations = Euclidean distance between the Location and the subtrahend.
         Only works if the subtrahend class is "Location". """
-        return math.sqrt((self.x - other_location.x) ** 2 + (self.y - other_location.y) ** 2)
+        if isinstance(subtrahend, Location):
+            dx = subtrahend.x
+            dy = subtrahend.y
+        else:  # assume the other class has an attribute "location" of class "Location"
+            dx = subtrahend.location.x
+            dy = subtrahend.location.y
+
+        return math.sqrt((self.x - dx) ** 2 + (self.y - dy) ** 2)
 
--- scripts/test/test_environment.py
+++ scripts/test/test_environment.py
@@ -7,6 +7,7 @@ sys.path.append('/nfs/attic/smartw/users/ruebenm/workspaces/exploration/src/clas
 from environment import Location, Bounds2D, World
 
 if __name__ == "__main__":
+
     # Test Location class
     a = Location(x=1, y=-3)
     b = Location(x=-1, y=1)
@@ -15,17 +16,20 @@ if __name__ == "__main__":
     print 'Should be 4.47:', a - b
     print 'Should be True:', b == c
 
-
     # Test World class (and POI, 2DBounds classes by extension)
     world_bounds = Bounds2D((0, 115), (0, 100))
-    world_center = (60, 50)
+    world_center = Location(60, 50)
     poi_ranges = (70, 70)
-    poi_bounds = Bounds2D((world_center[0]-poi_ranges[0]/2, world_center[0]+poi_ranges[0]/2), 
-                          (world_center[1]-poi_ranges[1]/2, world_center[1]+poi_ranges[1]/2))
+    poi_bounds = Bounds2D((world_center.x-poi_ranges[0]/2, world_center.x+poi_ranges[0]/2), 
+                          (world_center.y-poi_ranges[1]/2, world_center.y+poi_ranges[1]/2))
     print 'Should be ((25, 95), (15, 85)):', poi_bounds
-    world = World(world_bounds, 100, poi_bounds)
+    world = World(world_bounds, 100, poi_bounds, 30, world_center)
     import time
-    for i in range(10):
+    for i in range(5):
         world.reset()
         world.test_plot()
         time.sleep(0.5)
+    reward, rover_closest_list = world.get_rewards()
+    print 'Global reward:', reward
+    world.test_plot(rover_closest_list)
+    input('Press RETURN to quit. ')
